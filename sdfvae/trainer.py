7e5c98fe0df2a0b54e5ff0897fd0614bcc58c916
dlagul <dailiang@iie.ac.cn>
2021-03-11 01:03:07 +0800

    Update trainer.py

--- sdfvae/trainer.py
+++ sdfvae/trainer.py
@@ -63,7 +63,7 @@ class Trainer(object):
         # = log{1/(sqrt(2*pi)*sigma)exp{-(x-mu)^2/(2*sigma^2)}} 
         # = -0.5*{log(2*pi)+2*log(sigma)+[(x-mu)/exp{log(sigma)}]^2}
         # Note that var = sigma^2, i.e., log(var) = 2*log(sigma),
-        # so here the “recon_seq_logvar” is more appropriate to be called “recon_seq_logsigma”, but the name does not the matter
+        # so the “recon_seq_logvar” here is more appropriate to be called “recon_seq_logsigma”, but the name does not the matter
         loglikelihood = -0.5 * torch.sum(torch.pow(((original_seq.float()-recon_seq_mu.float())/torch.exp(recon_seq_logvar.float())), 2) 
                                          + 2 * recon_seq_logvar.float() 
                                          + np.log(np.pi*2))
